54d3c4016db55b46e1ad6a3d5ab8e7d569400ba4
ssrb <sb@unhexhexium.net>
2018-10-03 11:56:27 +1300

    cmake

--- thirdparty/cmake/SDL_image.cmake
+++ thirdparty/cmake/SDL_image.cmake
@@ -2,27 +2,6 @@ cmake_minimum_required(VERSION 3.8)
 
 project(SDL_image VERSION 1.0.0.0 LANGUAGES C CXX)
 
-#LOAD_BMP
-#LOAD_GIF
-#LOAD_JPG
-#LOAD_JPG_DYNAMIC="libjpeg-9.dll"
-#LOAD_LBM
-#LOAD_PCX
-#LOAD_PNG
-#LOAD_PNG_DYNAMIC="libpng16-16.dll"
-#LOAD_PNM
-#LOAD_SVG
-#LOAD_TGA
-#LOAD_TIF
-#LOAD_TIF_DYNAMIC="libtiff-5.dll"
-#LOAD_WEBP
-#LOAD_WEBP_DYNAMIC="libwebp-7.dll"
-#LOAD_XPM
-#LOAD_XV
-#PNG_USE_DLL
-#ZLIB_DLL
-
-
 set (SDL_image_SOURCES
     "IMG_bmp.c"
     "IMG_gif.c"
@@ -52,16 +31,44 @@ set (SDL_image_PUBLIC_HEADERS
     "SDL_image.h"
 )
 
-
-
-#set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake")
 set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "${CMAKE_SOURCE_DIR}/../build/cmake")
 find_package(SDL2 REQUIRED)
 
 
 add_library(${PROJECT_NAME} SHARED ${SDL_image_SOURCES} ${SDL_image_PRIVATE_HEADERS} ${SDL_image_PUBLIC_HEADERS})
-#set_target_properties(${PROJECT_NAME} PROPERTIES LINKER_LANGUAGE C)
-target_include_directories(${PROJECT_NAME} PRIVATE SDL2::SDL2)
+
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_BMP)
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_GIF)
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_JPG)
+if (WIN32)
+	target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_JPG_DYNAMIC="libjpeg-9.dll")
+endif()
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_LBM)
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_PCX)
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_PNG)
+if (WIN32)
+	target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_PNG_DYNAMIC="libpng16-16.dll")
+endif()
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_PNM)
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_SVG)
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_TGA)
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_TIF)
+if (WIN32)
+	target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_TIF_DYNAMIC="libtiff-5.dll")
+endif()
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_WEBP)
+if (WIN32)
+	target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_WEBP_DYNAMIC="libwebp-7.dll")
+endif()
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_XPM)
+target_compile_definitions(${PROJECT_NAME} PRIVATE LOAD_XV)
+target_compile_definitions(${PROJECT_NAME} PRIVATE PNG_USE_DLL)
+target_compile_definitions(${PROJECT_NAME} PRIVATE ZLIB_DLL)
+
+if (WIN32)
+	target_include_directories(${PROJECT_NAME} PRIVATE "${CMAKE_SOURCE_DIR}/VisualC/external/include")
+endif()
+
 target_link_libraries(${PROJECT_NAME} PRIVATE SDL2::SDL2)
 
 set (showimage_SOURCES
